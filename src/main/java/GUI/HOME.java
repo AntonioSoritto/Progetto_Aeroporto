499fee5fa702ff8d018be75fbb001740f3144b0b
AntonioSoritto <antonio_soritto2016@libero.it>
2025-05-16 20:19:29 +0200

    asdfghj

---
+++ src/main/java/GUI/HOME.java
@@ -0,0 +1,11 @@
+package GUI;
+
+import javax.swing.*;
+
+public class HOME {
+    private JLabel Messaggio;
+    private JComboBox comboBox1;
+    private JTextField textField1;
+    private JPasswordField passwordField1;
+    private JButton button1;
+}
---
+++ src/main/java/GUI/UTENTE.java
@@ -0,0 +1,9 @@
+package GUI;
+
+import javax.swing.*;
+
+public class UTENTE {
+    private JButton effettuaPrenotazioneButton;
+    private JComboBox comboBox1;
+    private JTextField textField1;
+}
