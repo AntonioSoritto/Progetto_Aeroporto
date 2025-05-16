35cf755e28bd6ab818f20ef75b48e62c335eb2dd
SBINNONNA <karimessimax@gmail.com>
2025-05-16 17:36:01 +0200

    GUI

---
+++ src/main/java/gui/Home.java
@@ -0,0 +1,34 @@
+package gui;
+
+import controller.Controller;
+
+import javax.swing.*;
+
+public class Home {
+    private JPanel mainPanel;
+    private static JFrame frameHome;
+    private Controller controller;
+    private JPanel Home;
+    private JButton VOLA;
+
+    public static void main(String[] args) {
+        frameHome = new JFrame("Home");
+        frameHome.setContentPane(new Home().mainPanel);
+        frameHome.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frameHome.pack();
+        frameHome.setVisible(true);
+
+
+    }
+
+    public Home() {
+        controller = new Controller();
+        // Add action listeners or other initialization code here
+
+    }
+
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here 
+    }
+}
