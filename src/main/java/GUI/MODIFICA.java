d9f52052d455d209e6506d29147fa4ab4d017386
AntonioSoritto <antonio_soritto2016@libero.it>
2025-05-17 13:11:06 +0200

    commit

---
+++ src/main/java/GUI/MODIFICA.java
@@ -0,0 +1,12 @@
+package GUI;
+
+import javax.swing.*;
+
+public class MODIFICA {
+    private JTextField IMPOSTANUOVOGATETextField;
+    private JComboBox comboBox2;
+    private JComboBox comboBox4;
+    private JComboBox comboBox3;
+    private JComboBox comboBox1;
+    private JButton confermaButton;
+}
