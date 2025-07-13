e32e1526a3ae0f83b51dbb2d7037055a07388cf5
Carmine Onorato <152515492+SBINNONNA@users.noreply.github.com>
2025-07-13 15:46:14 +0200

    commit finale

--- src/main/java/model/UtenteGenerico.java
+++ src/main/java/model/UtenteGenerico.java
@@ -1,12 +1,9 @@
 package model;
 /**
  * Rappresenta un utente generico del sistema, estende la classe {@link Utente}
- * aggiungendo il campo 'nome' per identificare l'utente.
  */
 
 public class UtenteGenerico extends Utente {
-    /** Nome dell'utente */
-
 
     /**
      * Costruttore della classe UtenteGenerico.
